45fd77f431f484b4783cfd76f784d2b5bf5466e9
Ubuntu <ubuntu@ip-172-31-90-22.ec2.internal>
2021-11-17 18:39:51 +0000

    added Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ *  Prints "Hello, World". 
+ *
+ *  % java HelloWorld
+ *  Hello, World
+ *
+ *  @author: Matthew Sanii
+ *  @version: 1.0
+ *  @since 2020-11-17
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}
